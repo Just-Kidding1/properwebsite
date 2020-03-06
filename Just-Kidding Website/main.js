1188337574f2b150e4903d25562fb00a31950061
Jason Duncan <59481139+Dragon4000@users.noreply.github.com>
2020-03-06 10:39:30 -0600

    Add files via upload

---
+++ Just-Kidding Website/main.js
@@ -0,0 +1,7 @@
+function Button(){
+    let button = document.getElementById("button0");
+    Name1 =  'Jason' ;
+    Name2 =  'Kevin' ;
+    alert(`this was created by ${Name1} and ${Name2}.`);
+//returns “Jason works for Dunder Neutral.”
+}
