bb1b58716a00c41739d0285f80b871c58ff0756d
Kevin Moore <59481191+Kevin539@users.noreply.github.com>
2020-03-13 10:07:54 -0500

    Add files via upload

--- Just-Kidding Website/main.js
+++ Just-Kidding Website/main.js
@@ -1,7 +1,10 @@
 function Button(){
-    let button = document.getElementById("button0");
-    Name1 =  'Jason' ;
-    Name2 =  'Kevin' ;
-    alert(`this was created by ${Name1} and ${Name2}.`);
-//returns “Jason works for Dunder Neutral.”
+    let button = document.getElementById("button");
+
+    str1 = "Jason and Kevin created this website."
+
+alert(str1);
 }
+
+
+
